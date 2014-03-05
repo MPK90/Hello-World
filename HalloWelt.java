6cf04a4f2e1b63ee05e1074c1402606b99950512
MPK90 <Mystryl@gmx.de>
2014-03-05 19:00:44 +0100

    Update HalloWelt.java
    Code wurde kommentiert

--- HalloWelt.java
+++ HalloWelt.java
@@ -3,6 +3,6 @@ package test;
 public class HalloWelt {
 
 	public static void main(String[] args){
-		System.out.println("Hallo Welt");
+		System.out.println("Hallo Welt"); //Hallo Welt wird ausgegeben
 	}
 }
